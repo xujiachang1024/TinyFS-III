024a5cd6efd1e42dc905c0e15b3ba7242d0db88b
hyunbumy <hyunbumy@usc.edu>
2018-04-22 16:03:37 -0700

    Implement RID

--- src/com/client/RID.java
+++ src/com/client/RID.java
@@ -1,5 +1,19 @@
 package com.client;
 
 public class RID {
+	private String chunkHandle;
+	private int slotID;
 	
+	public String getChunkHandle() {
+		return chunkHandle;
+	}
+	public void setChunkHandle(String chunkHandle) {
+		this.chunkHandle = chunkHandle;
+	}
+	public int getSlotID() {
+		return slotID;
+	}
+	public void setSlotID(int slotID) {
+		this.slotID = slotID;
+	}
 }
